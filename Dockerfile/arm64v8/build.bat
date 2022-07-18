acc4ee0bb66b174dd692c32fa7bb6ccea71859da
kaspersigi <kaspersigi@gmail.com>
2022-07-19 01:54:18 +0800

    update Dockerfile

--- Dockerfile/arm64v8/build.bat
+++ Dockerfile/arm64v8/build.bat
@@ -1,3 +1,3 @@
 docker buildx build --platform=linux/arm64v8 -t kaspersigi/clang:arm64v8 .
-docker run -it --privileged -p 2222:22 -v /d/arm64:/home/miku/linux --name=arm64--hostname Arm64-Miku kaspersigi/clang:arm64v8
+docker run -it --privileged -p 2222:22 -v /d/arm64:/home/miku/linux --name=arm64 --hostname Arm64-Miku kaspersigi/clang:arm64v8
 pause
