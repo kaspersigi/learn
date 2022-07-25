78f7914e52de32dfb721c708bd703ff62d949465
kaspersigi <kaspersigi@gmail.com>
2022-07-25 21:15:37 +0800

    update floppy.s

--- Learning Operating System/00.template/floppy.s
+++ Learning Operating System/00.template/floppy.s
@@ -8,6 +8,15 @@ mov byte [0x04], 'm'
 mov byte [0x05], 0x07
 mov byte [0x06], ' '
 mov byte [0x07], 0x07
+
+;写2000个空格
+mov bx, 0x06
+mov cx, 2000
+cls:
+mov word[bx], 0x0720
+add bx, 2
+loop cls
+
 jmp $
 times 510-($-$$) db 0
 db 0x55, 0xaa
