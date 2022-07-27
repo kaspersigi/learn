fa84cf0b6aeb69c20c9d8a6829cc82bd7b344f2c
kaspersigi <kaspersigi@gmail.com>
2022-07-28 00:00:11 +0800

    add gas protected mode

--- Learning Operating System/02.asm/main.s
+++ Learning Operating System/02.asm/main.s
@@ -5,18 +5,18 @@ _start:
 .code16
     movw $0xb800, %ax
     movw %ax, %ds
-    movb $'a', %ds:(0x00)
-    movb $0x07, %ds:(0x01)
-    movb $'s', %ds:(0x02)
-    movb $0x07, %ds:(0x03)
-    movb $'m', %ds:(0x04)
-    movb $0x07, %ds:(0x05)
+    movb $'a', (0x00)
+    movb $0x07, (0x01)
+    movb $'s', (0x02)
+    movb $0x07, (0x03)
+    movb $'m', (0x04)
+    movb $0x07, (0x05)
 
     # 写2000个空格
     movw $0x06, %bx
     movw $2000, %cx
     .cls:
-    movw $0x0720, %ds:(%bx)
+    movw $0x0720, (%bx)
     addw $2, %bx
     loop .cls
 
