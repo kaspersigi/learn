6c2d65de8cb20c303c586c7f570961109f626b18
kaspersigi <kaspersigi@gmail.com>
2022-08-08 22:11:13 +0800

    add driver

---
+++ Learning C/50.driver/hello_world.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int hello_init(void)
+{
+    printk(KERN_ALERT "Hello World!\n");
+    return 0;
+}
+
+static int hello_exit(void)
+{
+    printk(KERN_ALERT "Goodbye!\n");
+    return 0;
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+MODULE_LICENSE("GPL");
