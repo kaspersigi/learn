ecc9b3baddad636dfaea560fd72d0dce5d46c70a
kaspersigi <kaspersigi@gmail.com>
2022-09-21 02:03:51 +0800

    update link script

--- Learning Operating System/04.kernel/head.s
+++ Learning Operating System/04.kernel/head.s
@@ -3,7 +3,7 @@
 .equ kernel_start_sector,   0x00000004 # 常数，kernel起始逻辑扇区号
 
 # 定义引导程序头部段
-.section .head
+.section ".data.head"
 .align 4
     program_length: .int    program_end # 程序总长度
     text_entry:     .short  _start      # 代码段入口段内偏移地址
@@ -14,21 +14,23 @@
     text_segment:   .int    _start    # 代码段距程序首地址偏移地址
     realloc_tbl_end:
 
-.section .data
-.align 4
-_data_start:
-    .asciz "section data"
-
-.section .stack
-.align 4
-_stack_start:
-    .asciz "section stack"
-
 .section .text
 .align 4
 .global _start
 _start:
 .code16
     .asciz "section text"
-    movw $250, %ax
+
+.section .data
+.align 4
+_data_start:
+    .asciz "section data"
+
+.section ".data.stack"
+.align 4
+_stack_start:
+    .asciz "section stack"
+
+.section ".data.tail"
+    .asciz "section tail"
     program_end:
