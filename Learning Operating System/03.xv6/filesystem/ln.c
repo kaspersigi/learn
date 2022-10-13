b06fe007fe7c1c86126dc0f1a9ff967b879806f3
kaspersigi <kaspersigi@gmail.com>
2022-10-13 13:22:12 +0800

    update xv6

---
+++ Learning Operating System/03.xv6/filesystem/ln.c
@@ -0,0 +1,14 @@
+#include "../include/stat.h"
+#include "../include/types.h"
+#include "include/user.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
+        printf(2, "Usage: ln old new\n");
+        exit();
+    }
+    if (link(argv[1], argv[2]) < 0)
+        printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+    exit();
+}
