73a7ce6c405a921b84181cb359874cc39055ab03
kaspersigi <kaspersigi@gmail.com>
2022-10-30 12:45:32 +0800

    update valgrind

--- Learning Native/05.malloc/main.c
+++ Learning Native/05.malloc/main.c
@@ -1,10 +1,13 @@
+#include <mcheck.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
+    mtrace();
     char* str = malloc(13 * sizeof(char));
     free(str);
+    muntrace();
 
     return 0;
 }
--- Learning Native/06.new/main.cc
+++ Learning Native/06.new/main.cc
@@ -1,9 +1,15 @@
 #include <iostream>
 
+extern "C" {
+#include <mcheck.h>
+}
+
 int main(int argc, char* argv[])
 {
+    mtrace();
     char* str = new char[13];
     delete[] str;
+    muntrace();
 
     return 0;
 }
