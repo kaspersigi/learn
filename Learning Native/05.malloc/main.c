650133d1407499b8078ad78438844a5ee5335266
kaspersigi <kaspersigi@gmail.com>
2022-11-03 13:13:07 +0800

    Learing Native

--- Learning Native/05.malloc/main.c
+++ Learning Native/05.malloc/main.c
@@ -1,13 +1,22 @@
-#include <mcheck.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// sizeof(Chunk) == 4;
+typedef struct Chunk {
+    int length;
+    char data[0];
+} Chunk;
 
 int main(int argc, char* argv[])
 {
-    mtrace();
-    char* str = malloc(13 * sizeof(char));
-    free(str);
-    muntrace();
+    const char* str = "Hello World";
+    write(1, "begin", 5);
+    Chunk* my_chunk = malloc(sizeof(Chunk) + 12);
+    write(1, "end", 3);
+    my_chunk->length = 12;
+    memcpy(my_chunk->data, str, 12);
+    free(my_chunk);
 
     return 0;
 }
--- Learning Native/06.new/main.cc
+++ Learning Native/06.new/main.cc
@@ -1,15 +1,16 @@
-#include <iostream>
-
 extern "C" {
-#include <mcheck.h>
+#include <string.h>
+#include <unistd.h>
 }
 
 auto main(int argc, char* argv[]) -> int
 {
-    mtrace();
-    char* str = new char[13];
-    delete[] str;
-    muntrace();
+    const char* str = "Hello World";
+    write(1, "begin", 5);
+    char* my_str = new char[12];
+    write(1, "end", 3);
+    memcpy(my_str, str, 12);
+    delete[] my_str;
 
     return 0;
 }
