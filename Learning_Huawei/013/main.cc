3fe09d90290646196bd8f3eaa9130f0ccbafe930
kaspersigi <kaspersigi@gmail.com>
2023-04-06 13:15:41 +0800

    update 013

--- Learning_Huawei/013/main.cc
+++ Learning_Huawei/013/main.cc
@@ -2,11 +2,25 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+
+#if 0
+auto main(int argc, char* argv[]) -> int
+{
+    std::string word {};
+    while (std::cin >> word) {
+        std::vector<std::string> vs {};
+        vs.push_back(word);
+        std::for_each(vs.cbegin(), vs.cend(), [](auto e) { std::cout << e << " "; });
+        std::cout << std::endl;
+    }
+    return 0;
+}
+#endif
+
+#if 1
 extern "C" {
 #include <string.h>
 }
-
-#if 1
 auto main(int argc, char* argv[]) -> int
 {
     constexpr size_t MAX_SIZE = 1001;
@@ -20,4 +34,11 @@ auto main(int argc, char* argv[]) -> int
     }
     return 0;
 }
+#endif
+
+#if 0
+如何对std::string做split?
+std::cin //不太行
+std::istringstream
+std::regex
 #endif
