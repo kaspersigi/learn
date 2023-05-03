5bbbd07f9cd72c26c627fddabd4069895667d7a9
kaspersigi <kaspersigi@gmail.com>
2023-05-03 11:13:00 +0800

    update spiral_matrix_ii

--- Learning_Algorithm/05.spiral_matrix_ii/main.cc
+++ Learning_Algorithm/05.spiral_matrix_ii/main.cc
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> generate_matrix(int n)
+{
+    std::vector<std::vector<int>> vii(n, std::vector<int>(n, 0));
+
+    return vii;
+}
 
 auto main(int argc, char* argv[]) -> int
 {
-    std::cout << "Hello World!" << std::endl;
+    int n = 5;
+    auto vii = generate_matrix(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << vii[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
