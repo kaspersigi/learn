ba48da7f23014f37d66116d50e62e1e240d4bae5
kaspersigi <kaspersigi@gmail.com>
2023-05-09 23:10:27 +0800

    update 3sum

--- Learning_Algorithm/19.3sum/main.cc
+++ Learning_Algorithm/19.3sum/main.cc
@@ -1,15 +1,10 @@
 #include <algorithm>
 #include <iostream>
-#include <unordered_map>
 #include <vector>
 
 std::vector<std::vector<int>> three_sum(std::vector<int>& nums)
 {
     std::vector<std::vector<int>> vii {};
-    std::unordered_map<int, int> mi {};
-    for (int i = 0; i < nums.size(); ++i) {
-        mi[nums[i]] = i;
-    }
     return vii;
 }
 
