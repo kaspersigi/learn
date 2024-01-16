5803f6f82291dcf65a1bd5ce1b1cd74672e59e77
kaspersigi <kaspersigi@gmail.com>
2024-01-16 11:26:34 +0800

    update

--- Dockerfile/linux.sh
+++ Dockerfile/linux.sh
@@ -15,7 +15,6 @@ apt install default-jdk -y
 
 su miku
 cd ~/linux
-mkdir Learning_ARM
 
 # git clone --depth 1 git@github.com:oracle/opengrok.git
 wget https://github.com/oracle/opengrok/releases/download/1.13.0/opengrok-1.13.0.tar.gz
@@ -24,11 +23,12 @@ git clone --depth 1 git@github.com:universal-ctags/ctags.git
 cd ctags
 ./autogen.sh
 ./configure
-make -j16
+make -j$(nproc)
 sudo make install
 
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.17/bin/apache-tomcat-10.1.17.tar.gz
 
+git clone --single-branch -b linux-msft-wsl-6.1.y --depth 1 https://github.com/microsoft/WSL2-Linux-Kernel.git
 # git clone --single-branch -b linux-6.6.y --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
 git clone --single-branch -b linux-6.6.y --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
 # git clone --single-branch -b 1_36_stable --depth 1 git://git.busybox.net/busybox
@@ -36,21 +36,29 @@ git clone --single-branch -b 1_36_stable --depth 1 https://git.busybox.net/busyb
 git clone --single-branch -b u-boot-2023.07.y --depth 1 git@github.com:u-boot/u-boot.git
 # git clone --single-branch -b u-boot-2023.07.y --depth 1 https://source.denx.de/u-boot/u-boot.git
 
+mv WSL2-Linux-Kernel linux
+cd linux
+cp arch/x86/configs/config-wsl .config
+make menuconfig
+make -j$(nproc)
+sudo make modules_install headers_install
+cp arch/x86/boot/bzImage /mnt/d/Learning_Kernel/bzImage
+
 cd linux
 make vexpress_defconfig ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 make menuconfig ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 # File System —> Network File Systems —> NFS client support for NFS version 4
 # open all nfs client switch
-make -j16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
-make modules -j16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
-make dtbs -j16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
-make LOADADDR=0x60003000 uImage -j16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
-make modules_install INSTALL_MOD_PATH=../_fs -j16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+make modules -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+make dtbs -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+make LOADADDR=0x60003000 uImage -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+make modules_install INSTALL_MOD_PATH=../_fs -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 cd ..
 
 cd busybox
 make menuconfig ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
-make -j16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 make install ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- CONFIG_PREFIX=../_fs
 cp -R -d /usr/arm-linux-gnueabi/lib ../_fs
 cd ..
@@ -60,7 +68,7 @@ make vexpress_ca9x4_defconfig ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 make menuconfig ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 # Networking Support —> set a default gateway...
 # Boot options —> Enable a default value for bootcmd
-make -j16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 cd ..
 
 # tftp 0x60003000 linux/arch/arm/boot/uImage; tftp 0x60800000 linux/arch/arm/boot/dts/arm/vexpress-v2p-ca9.dtb; setenv bootargs 'root=/dev/nfs rw nfsroot=192.168.204.133:/home/miku/linux/filesystem,proto=tcp,nfsvers=3,nolock init=/linuxrc ip=192.168.204.200 console=ttyAMA0';bootm 0x60003000 - 0x60800000;
--- Dockerfile/root.sh
+++ Dockerfile/root.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
+# ubuntu 24.04
 
 apt update
 apt upgrade -y
 
-apt install clang-16 -y
-apt install clangd-16 -y
-apt install clang-format-16 -y
-apt install lldb-16 -y
-apt install libc++-16-dev -y
-apt install libc++abi-16-dev -y
-apt install clangd-16 -y
-apt install lld-16 -y
+apt install clang-17 -y
+apt install clangd-17 -y
+apt install clang-format-17 -y
+apt install lldb-17 -y
+apt install libc++-17-dev -y
+apt install libc++abi-17-dev -y
+apt install clangd-17 -y
+apt install lld-17 -y
 apt install make -y
 apt install bsdmainutils -y
 apt install qemu-system -y
@@ -24,25 +25,25 @@ apt install binutils-i686-linux-gnu -y
 apt install gcc-i686-linux-gnu -y
 apt install fonts-firacode -y
 
-# apt install clang-16 clangd-16 clang-format-16 lldb-16 libc++-16-dev libc++abi-16-dev make bsdmainutils qemu-system gdb cgdb valgrind libltdl7 libsdl2-2.0-0 libgtk2.0-0 binutils-i686-linux-gnu gcc-i686-linux-gnu fonts-firacode -y
+# apt install clang-17 clangd-17 clang-format-17 lldb-17 libc++-17-dev libc++abi-17-dev make bsdmainutils qemu-system gdb cgdb valgrind libltdl7 libsdl2-2.0-0 libgtk2.0-0 binutils-i686-linux-gnu gcc-i686-linux-gnu fonts-firacode -y
 apt install gcc make bzip2 libssl-dev libncurses-dev libelf-dev flex bison bc dwarves -y
 
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-16 100
-update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-16 100
-update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-16 100
-update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-16 100
-update-alternatives --install /usr/bin/lld lld /usr/bin/lld-16 100
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-17 100
+update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-17 100
+update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-17 100
+update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-17 100
+update-alternatives --install /usr/bin/lld lld /usr/bin/lld-17 100
 
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-16 100
-update-alternatives --install /usr/bin/ld.lld ld.lld /usr/bin/ld.lld-16 100
-update-alternatives --install /usr/bin/llvm-ar llvm-ar /usr/bin/llvm-ar-16 100
-update-alternatives --install /usr/bin/llvm-nm llvm-nm /usr/bin/llvm-nm-16 100
-update-alternatives --install /usr/bin/llvm-objcopy llvm-objcopy /usr/bin/llvm-objcopy-16 100
-update-alternatives --install /usr/bin/llvm-objdump llvm-objdump /usr/bin/llvm-objdump-16 100
-update-alternatives --install /usr/bin/llvm-readelf llvm-readelf /usr/bin/llvm-readelf-16 100
-update-alternatives --install /usr/bin/llvm-strip llvm-strip /usr/bin/llvm-strip-16 100
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-17 100
+update-alternatives --install /usr/bin/ld.lld ld.lld /usr/bin/ld.lld-17 100
+update-alternatives --install /usr/bin/llvm-ar llvm-ar /usr/bin/llvm-ar-17 100
+update-alternatives --install /usr/bin/llvm-nm llvm-nm /usr/bin/llvm-nm-17 100
+update-alternatives --install /usr/bin/llvm-objcopy llvm-objcopy /usr/bin/llvm-objcopy-17 100
+update-alternatives --install /usr/bin/llvm-objdump llvm-objdump /usr/bin/llvm-objdump-17 100
+update-alternatives --install /usr/bin/llvm-readelf llvm-readelf /usr/bin/llvm-readelf-17 100
+update-alternatives --install /usr/bin/llvm-strip llvm-strip /usr/bin/llvm-strip-17 100
 
-update-alternatives --install /usr/bin/cc cc /usr/bin/clang-16 100
-update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-16 100
+update-alternatives --install /usr/bin/cc cc /usr/bin/clang-17 100
+update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-17 100
 
 su miku
