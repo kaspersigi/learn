d224430650757331c9e8de8e0e471a6b0110f72c
kaspersigi <kaspersigi@outlook.com>
2024-05-05 08:03:29 +0000

    update always restart

--- Dockerfile/amd64/build.sh
+++ Dockerfile/amd64/build.sh
@@ -1,2 +1,2 @@
 docker build -t kaspersigi/clang:amd64 .
-docker run -it --privileged -p 2222:22 -p 8888:8888 -v /root/share:/home/miku/share --name=amd64 --hostname AMD64-Miku kaspersigi/clang:amd64
+docker run -it --restart always --privileged -p 2222:22 -p 8888:8888 -v /root/share:/home/miku/share --name=amd64 --hostname AMD64-Miku kaspersigi/clang:amd64
