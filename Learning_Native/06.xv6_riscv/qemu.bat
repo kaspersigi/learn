a8fd6fbd95729dc456431b3b5f3209162ac5838a
kaspersigi <kaspersigi@outlook.com>
2024-06-14 19:18:00 +0000

    update qemu

---
+++ Learning_Native/06.xv6_riscv/qemu.bat
@@ -0,0 +1,1 @@
+qemu-system-riscv64 -machine virt -bios none -kernel kernel/kernel -m 128M -smp 4 -global virtio-mmio.force-legacy=false -drive file=filesystem/fs.img,if=none,format=raw,id=x0 -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0 -nographic
