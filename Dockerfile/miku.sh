ecf3baa5dfb067cc873bea0f6b3ed7ab18c1a204
kaspersigi <kaspersigi@outlook.com>
2024-07-11 01:22:14 +0800

    update adb

--- Dockerfile/miku.sh
+++ Dockerfile/miku.sh
@@ -17,7 +17,7 @@ cp .vimrc ~
 unzip /mnt/d/Learning_Kernel/tspi/platform-tools-latest-windows.zip -d /mnt/c/Users/kaspe/Downloads/
 echo "# Android Debug Bridge" >> ~/.bashrc
 # echo "export PATH=$PATH:/mnt/c/Users/kaspe/Downloads/platform-tools" >> ~/.bashrc
-echo "export PATH=/home/miku/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Users/kaspe/Downloads/platform-tools"
+echo "export PATH=/home/miku/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Users/kaspe/Downloads/platform-tools" >> ~/.bashrc
 echo "alias adb='adb.exe'" >> ~/.bashrc
 source ~/.bashrc
 
