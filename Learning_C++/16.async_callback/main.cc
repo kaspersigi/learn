af94e9758e2e07c6e7fea56d0299b760fd183fec
kaspersigi <kaspersigi@outlook.com>
2024-11-24 19:57:22 +0800

    add class_async

--- Learning_C++/16.async_callback/main.cc
+++
@@ -1,9 +0,0 @@
-#include "MyWorker.h"
-#include <memory>
-
-auto main(int argc, char* argv[]) -> int
-{
-    auto mw = std::make_shared<MyWorker>();
-    mw->execute(9, std::string("Hello world!"));
-    return 0;
-}
