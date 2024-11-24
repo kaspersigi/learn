e34ffee89385cedbeae90d4d9ffb90325a6438f0
kaspersigi <kaspersigi@outlook.com>
2024-11-24 11:25:50 +0800

    add class_async_callback

---
+++ Learning_C++/17.class_async_callback/MyWorker.cc
@@ -0,0 +1,35 @@
+#include "MyWorker.h"
+#include <future>
+#include <iostream>
+#include <thread>
+
+int ChildWorker::child_worker(int value, const std::string& str)
+{
+    for (size_t i = 0; i < 30; ++i) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << __PRETTY_FUNCTION__ << ": " << i << ", value = " << value << ", " << str << std::endl;
+    }
+    return 0;
+}
+
+int MainWorker::main_worker()
+{
+    for (size_t i = 0; i < 60; ++i) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << __PRETTY_FUNCTION__ << ": " << i << std::endl;
+    }
+    return 0;
+}
+
+void MainWorker::execute(int value, const std::string& str)
+{
+#if 0
+    std::future<int> ret1(std::async(&MainWorker::main_worker, this));
+    int ret2 = _cw->child_worker(value, str);
+    ret1.get();
+#else
+    std::future<int> ret1(std::async(&ChildWorker::child_worker, _cw, value, std::ref(str)));
+    int ret2 = main_worker();
+    ret1.get();
+#endif
+}
---
+++ Learning_C++/17.class_async_callback/MyWorker.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+class ChildWorker {
+public:
+    ChildWorker() = default;
+    ~ChildWorker() = default;
+
+    int child_worker(int value, const std::string& str);
+
+protected:
+    ChildWorker(const ChildWorker&) = delete;
+    ChildWorker(ChildWorker&&) = delete;
+    ChildWorker& operator=(const ChildWorker&) = delete;
+    ChildWorker& operator=(ChildWorker&&) = delete;
+};
+
+class MainWorker {
+public:
+    MainWorker() = default;
+    ~MainWorker() = default;
+
+    void execute(int value, const std::string& str);
+
+protected:
+    MainWorker(const MainWorker&) = delete;
+    MainWorker(MainWorker&&) = delete;
+    MainWorker& operator=(const MainWorker&) = delete;
+    MainWorker& operator=(MainWorker&&) = delete;
+
+private:
+    int main_worker();
+    std::shared_ptr<ChildWorker> _cw {};
+};
