6b201ac4a0ac5134fac3eb170a7be018462bc3c3
kaspersigi <kaspersigi@outlook.com>
2024-11-30 16:28:04 +0800

    update learning algo

--- Learning_Algorithm/01.binary_search/main.cc
+++ Learning_Algorithm/01.binary_search/main.cc
@@ -2,6 +2,23 @@
 #include <vector>
 
 #if 0
+二分查找
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+eg1:
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+eg2:
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+提示：
+1、你可以假设 nums 中的所有元素是不重复的。
+2、n 将在 [1, 10000]之间。
+3、nums 的每个元素都将在 [-9999, 9999]之间。
+#endif
+
+#if 0
 // 非递归 闭区间版本[]
 int binary_search(std::vector<int>& nums, int target)
 {
@@ -76,7 +93,14 @@ int binary_search(std::vector<int>& nums, int target, int left, int right)
 auto main(int argc, char* argv[]) -> int
 {
     int target = 9;
-    std::vector<int> vi { -1, 0, 3, 5, 9, 12 };
+    std::vector<int> vi {
+        -1,
+        0,
+        3,
+        5,
+        9,
+        12,
+    };
     // std::cout << binary_search(vi, target) << std::endl;
     std::cout << binary_search(vi, target, 0, vi.size() - 1) << std::endl;
     // std::cout << binary_search(vi, target, 0, vi.size()) << std::endl;
