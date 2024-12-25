e2ef7b5246a1ae86032bf654174f9add81789f0e
kaspersigi <kaspersigi@outlook.com>
2024-12-25 23:49:47 +0800

    update regex

--- Learning_C++/02.regex/main.cc
+++ Learning_C++/02.regex/main.cc
@@ -1,38 +1,34 @@
-#include <cstring>
+#include <fstream>
 #include <iostream>
 #include <regex>
 
-std::string str(R"((\d+\-\d+) +(\d+:\d+:\d+\.\d+) +(\d+) +(\d+) +([a-zA-Z]?) +([a-zA-Z0-9\.\-\?\\_@]+) *: +(.*)\n)");
+static const std::string str(R"(^(\d+\-\d+) +(\d+:\d+:\d+\.\d+) +(\d+) +(\d+) +([a-zA-Z]?) +([a-zA-Z0-9\.\-\?\\_@]+) *: +(.*)$)");
 
 auto main(int argc, char* argv[]) -> int
 {
-    FILE* origin_log_file = fopen("./mainlog.txt", "r");
-    if (NULL == origin_log_file)
-        perror("open mainlog file");
-
-    char buffer[1024] {};
+    std::ifstream ifs("mainlog.txt");
+    std::string line;
     std::regex pattern(str);
     std::smatch matched_part;
-    while (!feof(origin_log_file)) {
-        memset(buffer, 0, 1024);
-        fgets(buffer, 1024, origin_log_file);
-        std::string line = buffer;
-        bool found = std::regex_search(line, matched_part, pattern);
-        if (found) {
-            std::string date = matched_part[1];
-            std::string time = matched_part[2];
-            std::string pid = matched_part[3];
-            std::string tid = matched_part[4];
-            std::string level = matched_part[5];
-            std::string tag = matched_part[6];
-            std::string message = matched_part[7];
-            if (pid == "1144")
-                std::cout << tag << std::endl;
-            // std::string stand_time = "2022-" + date + " " + time;
-            // std::cout << stand_time << std::endl;
+    if (ifs.is_open()) {
+        while (std::getline(ifs, line)) {
+            // std::cout << line << std::endl;
+            bool found = std::regex_search(line, matched_part, pattern);
+            if (found) {
+                std::string date = matched_part[1];
+                std::string time = matched_part[2];
+                std::string pid = matched_part[3];
+                std::string tid = matched_part[4];
+                std::string level = matched_part[5];
+                std::string tag = matched_part[6];
+                std::string message = matched_part[7];
+                if (pid == "1144")
+                    std::cout << tag << std::endl;
+                // std::string stand_time = "2022-" + date + " " + time;
+                // std::cout << stand_time << std::endl;
+            }
         }
     }
-    fclose(origin_log_file);
 
     return 0;
 }
