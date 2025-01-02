6ac5cd25c5fd64ef3042751de2e4d4299a9f7d18
kaspersigi <kaspersigi@outlook.com>
2025-01-02 13:07:58 +0800

    update opencl

--- Learning_Android/08.android_opencl/main.c
+++ Learning_Android/08.android_opencl/main.c
@@ -15,28 +15,28 @@ static const size_t KNL_NUM = 1;
 
 int main(int argc, char* argv[])
 {
-    cl_int status = CL_SUCCESS;
+    cl_int err = CL_SUCCESS;
     cl_uint numPlatforms = 0;
 
-    status = clGetPlatformIDs(0, NULL, &numPlatforms);
+    err = clGetPlatformIDs(0, NULL, &numPlatforms);
 
-    if (status == CL_SUCCESS) {
+    if (err == CL_SUCCESS) {
         LOGI("%u platform(s) found", numPlatforms);
     } else {
-        LOGE("clGetPlatformIDs(%i)", status);
+        LOGE("clGetPlatformIDs(%i)", err);
         exit(-1);
     }
 
     cl_platform_id* PlatformIDs = (cl_platform_id*)calloc(numPlatforms, sizeof(cl_platform_id));
-    status = clGetPlatformIDs(numPlatforms, PlatformIDs, NULL);
+    err = clGetPlatformIDs(numPlatforms, PlatformIDs, NULL);
 
     for (cl_uint i = 0; i < numPlatforms; ++i) {
         cl_platform_id platformID = PlatformIDs[i];
         size_t size;
-        status = clGetPlatformInfo(platformID, CL_PLATFORM_NAME, 0, NULL, &size);
+        err = clGetPlatformInfo(platformID, CL_PLATFORM_NAME, 0, NULL, &size);
         char* name = calloc(size + 1, sizeof(char));
 
-        status = clGetPlatformInfo(platformID, CL_PLATFORM_NAME, size, name, NULL);
+        err = clGetPlatformInfo(platformID, CL_PLATFORM_NAME, size, name, NULL);
         LOGI("platformID name: %s", name);
         free(name);
         name = NULL;
@@ -44,9 +44,9 @@ int main(int argc, char* argv[])
 
     cl_uint numCPUDevices = 0;
     cl_uint numGPUDevices = 0;
-    status = clGetDeviceIDs(PlatformIDs[0], CL_DEVICE_TYPE_CPU, 0, NULL, &numCPUDevices);
+    err = clGetDeviceIDs(PlatformIDs[0], CL_DEVICE_TYPE_CPU, 0, NULL, &numCPUDevices);
     LOGI("platformIDs[0] %u cpu device(s) found", numCPUDevices);
-    status = clGetDeviceIDs(PlatformIDs[0], CL_DEVICE_TYPE_GPU, 0, NULL, &numGPUDevices);
+    err = clGetDeviceIDs(PlatformIDs[0], CL_DEVICE_TYPE_GPU, 0, NULL, &numGPUDevices);
     LOGI("platformIDs[0] %u gpu device(s) found", numGPUDevices);
 
     free(PlatformIDs);
