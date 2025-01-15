eb78cfeb93f4f55f06b354aea9d49949b1663cc5
kaspersigi <kaspersigi@outlook.com>
2025-01-15 21:34:53 +0800

    update tspi android config

--- Dockerfile/linux.sh
+++ Dockerfile/linux.sh
@@ -28,7 +28,20 @@ make modules -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=-18
 cp arch/arm64/boot/Image ../virt/
 make modules_install INSTALL_MOD_PATH=../virt/_fs -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=-18
 
-sudo apt install git make openjdk-8-jdk git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev libgl1-mesa-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libxml2-utils xsltproc unzip bc imagemagick ccache schedtool libssl-dev libncursesw5-dev libncurses5 libncursesw5 libncurses5-dev libncurses-dev libncurses-gst libncurses5-dev device-tree-compiler lz4 -y
+sudo apt install git make openjdk-8-jdk git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev libgl1-mesa-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libxml2-utils xsltproc unzip bc imagemagick ccache schedtool libssl-dev libncursesw5-dev libncurses5 libncursesw5 libncurses5-dev libncurses-dev libncurses-gst libncurses5-dev device-tree-compiler lz4 python3 python-is-python3 -y
+
+vim ./kernel/arch/arm64/configs/tspi_defconfig
+vim ./kernel/configs/rockchip/rk356x_evb.config
+vim ./kernel/configs/rockchip/android-11.config
+
+cd kernel
+echo '' >> ./arch/arm64/configs/tspi_defconfig
+echo CONFIG_DYNAMIC_FTRACE=y >> ./arch/arm64/configs/tspi_defconfig
+echo CONFIG_FUNCTION_TRACER=y >> ./arch/arm64/configs/tspi_defconfig
+echo CONFIG_IRQSOFF_TRACER=y >> ./arch/arm64/configs/tspi_defconfig
+echo CONFIG_FUNCTION_PROFILER=y >> ./arch/arm64/configs/tspi_defconfig
+echo CONFIG_PREEMPT_TRACER=y >> ./arch/arm64/configs/tspi_defconfig
+
 cd u-boot && ./make.sh rk3566 && cd ../kernel && make clean && make distclean && make ARCH=arm64 tspi_defconfig rk356x_evb.config android-11.config && make ARCH=arm64 tspi-rk3566-user-v10.img -j$(nproc) && cd .. && source build/envsetup.sh && lunch rk3566_tspi-userdebug && make installclean -j$(nproc) && make -j$(nproc) && ./mkimage.sh
 
 sudo apt install git ssh make gcc libssl-dev liblz4-tool expect g++ patchelf chrpath gawk texinfo chrpath diffstat binfmt-support qemu-user-static live-build bison flex fakeroot cmake gcc-multilib g++-multilib unzip device-tree-compiler ncurses-dev fdisk -y
