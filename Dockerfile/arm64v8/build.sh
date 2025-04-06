1d4ae079da5071f6f4b845b3d8bdc315955adcce
kaspersigi <kaspersigi@outlook.com>
2025-04-06 08:35:01 +0000

    update Dockerfile

--- Dockerfile/arm64v8/build.sh
+++ Dockerfile/arm64v8/build.sh
@@ -1,2 +1,3 @@
 docker build -t kaspersigizz/clang:arm64v8 .
-docker run -it --restart always --privileged -p 2222:22 -p 8888:8888 -v /root/share:/home/miku/share --name=arm64 --hostname Arm64-Miku kaspersigizz/clang:arm64v8
+docker run -it --restart always --privileged -p 2222:22 -p 8888:8888 -v /Users/miku/share:/home/miku/share --name=arm64 --hostname Arm64-Miku kaspersigizz/clang:arm64v8
+# docker run -it --restart always --privileged -p 2222:22 -p 8888:8888 -v /root/share:/home/miku/share --name=arm64 --hostname Arm64-Miku kaspersigizz/clang:arm64v8
