df0ddea65581d428ad04933aee48905db957aa49
kaspersigi <kaspersigi@outlook.com>
2025-04-14 21:18:49 +0800

    update threadpool

--- Learning_Multithread/06.threadpool/main.cc
+++ Learning_Multithread/06.threadpool/main.cc
@@ -1,93 +1,72 @@
-#include "ftrace.h"
 #include <condition_variable>
+#include <functional>
 #include <mutex>
 #include <print>
+#include <queue>
 #include <thread>
+#include <vector>
 
-// 最好改成回调，当callback为ok时，继续执行，否则阻塞至ok
-
-class Singleton {
+class SimpleThreadPool {
 public:
-    ~Singleton() = default;
-    static Singleton& GetInstance();
+    explicit SimpleThreadPool(size_t thread_count)
+        : stop(false)
+    {
+        for (size_t i = 0; i < thread_count; ++i) {
+            workers.emplace_back([this] {
+                while (true) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock lock(queue_mutex);
+                        condition.wait(lock, [this] {
+                            return stop || !tasks.empty();
+                        });
 
-protected:
-    Singleton() = default;
-    Singleton(const Singleton& obj) = delete;
-    Singleton(Singleton&& obj) = delete;
-    Singleton& operator=(const Singleton& obj) = delete;
-    Singleton& operator=(Singleton&& obj) = delete;
+                        if (stop && tasks.empty())
+                            return;
+
+                        task = std::move(tasks.front());
+                        tasks.pop();
+                    }
+                    task();
+                }
+            });
+        }
+    }
+
+    template <typename F>
+    void enqueue(F&& f)
+    {
+        {
+            std::unique_lock lock(queue_mutex);
+            tasks.emplace(std::forward<F>(f));
+        }
+        condition.notify_one();
+    }
+
+    ~SimpleThreadPool()
+    {
+        {
+            std::unique_lock lock(queue_mutex);
+            stop = true;
+        }
+        condition.notify_all();
+        for (auto& worker : workers) {
+            worker.join();
+        }
+    }
 
 private:
-    std::mutex _mutex;
-    std::condition_variable _cv;
-    bool is_finish { false };
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
 
-public:
-    void notify();
-    void wait();
-    void doing();
+    std::mutex queue_mutex;
+    std::condition_variable condition;
+    bool stop = false;
 };
-
-Singleton& Singleton::GetInstance()
-{
-    static Singleton instance;
-    return instance;
-}
-
-void Singleton::notify()
-{
-    _cv.notify_one();
-}
-
-void Singleton::wait()
-{
-    std::unique_lock<std::mutex> ul(_mutex);
-    _cv.wait(ul, [this]() { return is_finish; });
-}
-
-void Singleton::doing()
-{
-    std::println("{}: process E", __func__);
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    std::println("{}: process X", __func__);
-    {
-        std::lock_guard<std::mutex> lg(_mutex);
-        is_finish = true;
-    }
-}
-
-void func1()
-{
-    Ftrace::ftrace_duration_begin("ChildThread1");
-    std::println("{}: process E", __func__);
-    auto& s = Singleton::GetInstance();
-    s.doing();
-    s.notify();
-    std::println("{}: process X", __func__);
-    Ftrace::ftrace_duration_end();
-}
-
-void func2()
-{
-    Ftrace::ftrace_duration_begin("ChildThread2");
-    std::println("{}: process E", __func__);
-    auto& s = Singleton::GetInstance();
-    s.wait();
-    std::println("{}: process X", __func__);
-    Ftrace::ftrace_duration_end();
-}
 
 auto main(int argc, char* argv[]) -> int
 {
-    Ftrace::ftrace_duration_begin("MainThread");
-    std::println("{}: process E", __func__);
-    std::thread t1(func1);
-    std::thread t2(func2);
-    t1.detach();
-    t2.join();
-    std::println("{}: process X", __func__);
-    Ftrace::ftrace_duration_end();
+    std::print("Hello World!");
 
     return 0;
 }
