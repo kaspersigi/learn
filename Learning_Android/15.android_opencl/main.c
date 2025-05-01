ac2803ccdd1b0d747a39d5b9edc890353589dec4
kaspersigi <kaspersigi@outlook.com>
2025-05-01 23:30:34 +0800

    add opencl trace for mali gpu

--- Learning_Android/15.android_opencl/main.c
+++ Learning_Android/15.android_opencl/main.c
@@ -7,17 +7,25 @@
 
 void printDeviceInfo(cl_device_id device)
 {
+    int ret = 0;
     char device_name[128];
     char vendor_name[128];
 
+    ret = ftrace_duration_begin("clGetDeviceInfo");
     clGetDeviceInfo(device, CL_DEVICE_NAME, sizeof(device_name), device_name, NULL);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clGetDeviceInfo");
     clGetDeviceInfo(device, CL_DEVICE_VENDOR, sizeof(vendor_name), vendor_name, NULL);
+    ret = ftrace_duration_end();
 
     printf("Device: %s, Vendor: %s\n", device_name, vendor_name);
 }
 
 int my_cl_program()
 {
+    int ret = 0;
+
     cl_int err;
     cl_platform_id platform;
     cl_device_id device;
@@ -35,24 +43,42 @@ int my_cl_program()
     }
 
     // 1. 获取平台和设备
+    ret = ftrace_duration_begin("clGetPlatformIDs");
     err = clGetPlatformIDs(1, &platform, NULL);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clGetDeviceIDs");
     err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+    ret = ftrace_duration_end();
+
     printDeviceInfo(device);
 
     // 2. 创建OpenCL上下文和命令队列
+    ret = ftrace_duration_begin("clCreateContext");
     context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+    ret = ftrace_duration_end();
 
     // 使用新的API创建命令队列
     cl_queue_properties properties[] = { CL_QUEUE_PROPERTIES, 0, 0 };
+    ret = ftrace_duration_begin("clCreateCommandQueueWithProperties");
     queue = clCreateCommandQueueWithProperties(context, device, properties, &err);
+    ret = ftrace_duration_end();
 
     // 3. 创建内存缓冲区
+    ret = ftrace_duration_begin("clCreateBuffer");
     a_mem = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
         sizeof(float) * DATA_SIZE, a, &err);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clCreateBuffer");
     b_mem = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
         sizeof(float) * DATA_SIZE, b, &err);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clCreateBuffer");
     result_mem = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
         sizeof(float) * DATA_SIZE, NULL, &err);
+    ret = ftrace_duration_end();
 
     // 4. 创建内核程序
     const char* kernel_source = "__kernel void vector_add(__global const float *a, __global const float *b, __global float *result) { \n"
@@ -60,32 +86,55 @@ int my_cl_program()
                                 "    result[i] = a[i] + b[i]; \n"
                                 "} \n";
 
+    ret = ftrace_duration_begin("clCreateProgramWithSource");
     program = clCreateProgramWithSource(context, 1, &kernel_source, NULL, &err);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clBuildProgram");
     err = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+    ret = ftrace_duration_end();
 
     if (err != CL_SUCCESS) {
         char build_log[4096];
+
+        ret = ftrace_duration_begin("clGetProgramBuildInfo");
         clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG,
             sizeof(build_log), build_log, NULL);
+        ret = ftrace_duration_end();
+
         printf("Build error:\n%s\n", build_log);
         return -1;
     }
 
     // 5. 创建内核
+    ret = ftrace_duration_begin("clCreateKernel");
     kernel = clCreateKernel(program, "vector_add", &err);
+    ret = ftrace_duration_end();
 
     // 6. 设置内核参数
+    ret = ftrace_duration_begin("clSetKernelArg");
     err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &a_mem);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clSetKernelArg");
     err = clSetKernelArg(kernel, 1, sizeof(cl_mem), &b_mem);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clSetKernelArg");
     err = clSetKernelArg(kernel, 2, sizeof(cl_mem), &result_mem);
+    ret = ftrace_duration_end();
 
     // 7. 执行内核
     size_t global_size = DATA_SIZE;
+    ret = ftrace_duration_begin("clEnqueueNDRangeKernel");
     err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global_size, NULL, 0, NULL, NULL);
+    ret = ftrace_duration_end();
 
     // 8. 读取结果
+    ret = ftrace_duration_begin("clEnqueueReadBuffer");
     err = clEnqueueReadBuffer(queue, result_mem, CL_TRUE, 0,
         sizeof(float) * DATA_SIZE, results, 0, NULL, NULL);
+    ret = ftrace_duration_end();
 
     // 打印结果
     printf("Results:\n");
@@ -94,13 +143,33 @@ int my_cl_program()
     }
 
     // 9. 清理资源
+    ret = ftrace_duration_begin("clReleaseMemObject");
     clReleaseMemObject(a_mem);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clReleaseMemObject");
     clReleaseMemObject(b_mem);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clReleaseMemObject");
     clReleaseMemObject(result_mem);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clReleaseKernel");
     clReleaseKernel(kernel);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clReleaseProgram");
     clReleaseProgram(program);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clReleaseCommandQueue");
     clReleaseCommandQueue(queue);
+    ret = ftrace_duration_end();
+
+    ret = ftrace_duration_begin("clReleaseContext");
     clReleaseContext(context);
+    ret = ftrace_duration_end();
 
     return 0;
 }
@@ -114,6 +183,7 @@ int main(void)
     ret = ftrace_duration_begin("MainThread");
     ret = my_cl_program();
     ret = ftrace_duration_end();
+
     ftrace_close();
 
     return 0;
