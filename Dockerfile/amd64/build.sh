65b8a3f6e76924be13ace1a169ea2706883019f5
kaspersigi <kaspersigi@outlook.com>
2025-09-14 17:22:56 +0000

    update Dockerfile

--- Dockerfile/amd64/build.sh
+++ Dockerfile/amd64/build.sh
@@ -1,13 +1,13 @@
 docker build -t kaspersigizz/clang:amd64 .
 
-docker run -it \
+docker run -d \
   --restart always \
   --privileged \
   -p 2222:22 \
   -p 8888:8888 \
-  -e CODESERVER_PASSWORD=MyCodeServer           \  # 👈 设置你的密码
-  -v ~/.ssh:/home/miku/.ssh:ro \                   # 👈 挂载你的 SSH 密钥（只读）
-  -v ~/share:/home/miku/share \                    # 👈 挂载共享工作目录
-  --name dev-container \
+  -e CODESERVER_PASSWORD=MyCodeServer \
+  -v ~/.ssh:/home/miku/.ssh:ro \
+  -v ~/share:/home/miku/share \
+  --name AMD64-Miku \
   --hostname AMD64-Miku \
   kaspersigizz/clang:amd64
