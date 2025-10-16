01abbc35f37cf7995126f5b82bcf6006719790ee
kaspersigi <kaspersigi@outlook.com>
2025-10-16 12:14:54 +0000

    add nativetest

--- Dockerfile/miku.sh
+++ Dockerfile/miku.sh
@@ -61,6 +61,8 @@ git clone git@github.com:kaspersigi/learn.git
 # git clone https://github.com/kaspersigi/learn.git
 git clone git@github.com:kaspersigi/perf_tools.git
 # git clone https://github.com/kaspersigi/perf_tools.git
+git clone git@github.com:kaspersigi/nativetest.git
+# git clone https://github.com/kaspersigi/nativetest.git
 git clone git@github.com:kaspersigi/CAMERA.LA.5.0.r1-03800-pakala.0-1.git
 # git clone https://github.com/kaspersigi/CAMERA.LA.5.0.r1-03800-pakala.0-1.git
 git clone git@github.com:kaspersigi/Camera.git
