aa6b1b98e7132aab192b0c67165ee4c9365dbfc3
kaspersigi <kaspersigi@outlook.com>
2025-10-26 14:35:54 +0800

    update slice name

--- Learning_Driver/05.trace/include/trace/events/mychar.h
+++ Learning_Driver/05.trace/include/trace/events/mychar.h
@@ -12,11 +12,14 @@ TRACE_EVENT(char_slice,
     TP_PROTO(char type, const char* name),
     TP_ARGS(type, name),
     TP_STRUCT__entry(
-        __field(char, track_event_type)
-            __string(slice_name, name)),
+        __field(char, type)          // ← 关键：字段名必须是 "type"
+        __string(name, name)         // ← 字段名必须是 "name"
+    ),
     TP_fast_assign(
-        __entry->track_event_type = type;
-        __assign_str(slice_name, name);),
-    TP_printk("type=%c name=%s", __entry->track_event_type, __get_str(slice_name)));
+        __entry->type = type;
+        __assign_str(name, name);
+    ),
+    TP_printk("type=%c name=%s", __entry->type, __get_str(name))
+);
 
 #include <trace/define_trace.h>
